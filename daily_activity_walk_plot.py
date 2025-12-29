0a5c627de0edf9a8bf09591118e7ac05f55c22d4
Mark Lindsay <godisawsume@icloud.com>
2025-12-29 11:41:30 -0800

    Update daily_activity_walk_plot.py

--- daily_activity_walk_plot.py
+++ daily_activity_walk_plot.py
@@ -1,5 +1,13 @@
 <nav>
   <ul>
+    <li><a href="treadmill.html">Treadmill</a></li>
+    <li><a href="strength.html">Strength Training</a></li>
+    <li><a href="walk.html">Walks</a></li>
+  </ul>
+</nav>
+
+<nav>
+  <ul>
     <li><a href="daily_activity_treadmill.html">Treadmill</a></li>
     <li><a href="daily_activity_strength.html">Strength Training</a></li>
     <li><a href="daily_activity_walk.html">Walks</a></li>

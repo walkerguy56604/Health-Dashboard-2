5346a5136f022b558c5893c3423bd5528ba3159d
Mark Lindsay <godisawsume@icloud.com>
2025-12-29 11:39:22 -0800

    Update daily_activity_strength_plot.py

--- daily_activity_strength_plot.py
+++ daily_activity_strength_plot.py
@@ -1,7 +1,10 @@
+<!-- Navigation Menu -->
 <nav>
   <ul>
-    <li><a href="daily_activity_treadmill.html">Treadmill</a></li>
-    <li><a href="daily_activity_strength.html">Strength Training</a></li>
-    <li><a href="daily_activity_walk.html">Walks</a></li>
+    <li><a href="dashboard.html">Dashboard</a></li>
+    <li><a href="daily-activity-treadmill.html">Treadmill</a></li>
+    <li><a href="daily-activity-strength.html">Strength Training</a></li>
+    <li><a href="daily-activity-walk.html">Walk</a></li>
   </ul>
 </nav>
+<hr>
